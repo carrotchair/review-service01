3adf42edeef0243be21fea72a134c5951aed2d21
carrotchair <39000323+carrotchair@users.noreply.github.com>
2024-08-06 13:18:34 +0200

    Changes

--- movies/src/main/java/com/example/movies/model/Review.java
+++ movies/src/main/java/com/example/movies/model/Review.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
+    @Id
     private ObjectId id;
     private String body;
     private LocalDateTime created;
--- movies/src/main/java/com/example/movies/resolver/Query.java
+++ movies/src/main/java/com/example/movies/resolver/Query.java
@@ -1,24 +1,27 @@
-package com.example.movies.resolver;
-
-import graphql.kickstart.tools.GraphQLQueryResolver;
-import com.example.movies.model.Movie;
-import com.example.movies.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-public class Query implements GraphQLQueryResolver {
-    @Autowired
-    private MovieRepository movieRepository;
-
-    public List<Movie> getAllMovies2() {
-        return movieRepository.findAll();
-    }
-
-    public Movie getSingleMovie(String imdbId) {
-        return movieRepository.findMovieByImdbId(imdbId).orElse(null);
-    }
-
-}
+//package com.example.movies.resolver;
+//
+//import com.example.movies.model.Movie;
+//import com.example.movies.repository.MovieRepository;
+//import graphql.kickstart.tools.GraphQLQueryResolver;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Component
+//@Service
+//public class Query implements GraphQLQueryResolver {
+//
+//    @Autowired
+//    private MovieRepository movieRepository;
+//
+//    public List<Movie> getAllMovies2() {
+//        return movieRepository.findAll();
+//    }
+//
+//    public Movie getSingleMovie(String imdbId) {
+//        return movieRepository.findMovieByImdbId(imdbId).orElse(null);
+//    }
+//
+//}
